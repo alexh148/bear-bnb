9d6932e2a3e2d1ea1f22bb15eabe96856aaa21a5
Jedd Hopkins <jedd_hoppo@hotmail.com>
2019-02-20 16:34:14 +0000

    major commit

---
+++ client/src/components/Header/Add/index.js
@@ -0,0 +1,12 @@
+import React from'react';
+
+
+const Header = (props) => (
+  <div>
+     <h1> Create a Listing </h1>
+     <h2> Pick from our luxury listings </h2>
+
+  </div>
+)
+
+export default Header;
---
+++ client/src/components/MainBody/Add/index.js
@@ -0,0 +1,11 @@
+import React from'react';
+
+
+const AddMain = (props) => (
+  <div>
+  Something else
+
+  </div>
+)
+
+export default AddMain;
