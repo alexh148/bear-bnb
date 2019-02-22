8e9192b59b3c47ed198a836c750511eca86fc9d0
Jedd Hopkins <jedd_hoppo@hotmail.com>
2019-02-22 09:48:07 +0000

    Chloe made a picture good

--- client/src/components/navBar/index.js
+++ client/src/components/navBar/index.js
@@ -1,13 +1,16 @@
 import React from'react';
 
 const NavBar = (props) => (
+
   <header className="App-header">
+
   <ul>
+    <img id="headerlogo" src={require('./Bearbnb4.png')} />
     <li><a className="active" href="#home">Log In</a></li>
     <li><a href="#news">Sign Up</a></li>
     <li><a href="/spaces">View Listings</a></li>
     <li><a href="/spaces/new">Add Listing</a></li>
-    </ul>
+  </ul>
 
   </header>
 )
